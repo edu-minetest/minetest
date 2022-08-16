d0c0ad753eec158746163890679cbc0002a0de96
Riceball LEE <snowyu.lee@gmail.com>
2022-08-17 07:05:26 +0800

    fix(builtin): adjust lang dropdown position and size

--- builtin/mainmenu/tab_teacher.lua
+++ builtin/mainmenu/tab_teacher.lua
@@ -108,8 +108,8 @@ local function get_formspec(tabview, name, tabdata)
     "checkbox[0,1.7;allowLocalPlay;"..fgettext("Allow Local Play") ..";".. dump(settings.allowLocalPlay) .. "]" ..
     "checkbox[0,2.2;allowRemotePlay;"..fgettext("Allow Remote Play") ..";".. dump(settings.allowRemotePlay) .. "]" ..
     "label[0,2.9;" .. fgettext("Language") .."]" ..
-    "dropdown[0.8,3;1;lang;".. settings.langs ..";".. settings.langIndex .. ";false]" ..
-    "label[1.8,2.9;" .. langHint .."]" ..
+    "dropdown[1.8,2.8;1.6;lang;".. settings.langs ..";".. settings.langIndex .. ";false]" ..
+    "label[3.2,2.9;" .. langHint .."]" ..
     "button[8.9,5;3,0.8;btnStudentMode;".. fgettext("Student Mode") .."]" ..
     "button[5.9,5;3,0.8;btnSave;".. fgettext("Save") .."]"
 
